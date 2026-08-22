937c10bd64b10e6c19168a92cb0809838434c96e
eddydevst <toledoquirogaeddy@gmail.com>
2026-08-22 03:35:43 -0400

    Conexión a la BD

--- backend/app/config.py
+++ backend/app/config.py
@@ -12,7 +12,7 @@ class Config:
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-    SCHEMA='taller'
+    SCHEMA='obras'
     
     
     #CREDENCIALES CONFIGURACION APP
